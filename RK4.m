d96710ef220f0d33ff99514ef6bea3473dcf684c
JimmieD <james.dickey1@gmail.com>
2018-04-03 21:25:08 -0400

    Add files via upload

--- RK4.m
+++ RK4.m
@@ -1,5 +1,6 @@
 function output = RK4(t,X,h,tf)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%RK4(0,[1.939171267526330E3;7.053946077269909E3;5.216461164024867E3;7.728790449144201E-1;-5.188238121076679;3.991379730081099],60,60)
 % inputs:
 %           h - step size
 %           t - initial time value
@@ -25,8 +26,14 @@ for i = 2:1:tf
 Xn = A+1/6*(k1+2*k2+2*k3+k4);
 
 output(i,1:7) = [i,Xn(1,1),Xn(2,1),Xn(3,1),Xn(4,1),Xn(5,1),Xn(6,1)];
-
 end
+
+subplot(3,1,1);
+plot(output(:,1),output(:,2));
+subplot(3,1,2);
+plot(output(:,1),output(:,3));
+subplot(3,1,3);
+plot(output(:,1),output(:,4));
 
 
 end
